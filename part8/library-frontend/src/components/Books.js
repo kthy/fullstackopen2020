b1e7ea1151e0a797bf15f3b52f24e51bbd50c176
Kristian Thy <thy@le34.dk>
2020-09-02 09:30:49 +0200

    8.17 Listing books

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -26,7 +26,7 @@ const Books = (props) => {
           {props.result.data.allBooks.map(b =>
             <tr key={b.id}>
               <td>{b.title}</td>
-              <td>{b.author}</td>
+              <td>{b.author.name}</td>
               <td>{b.published}</td>
             </tr>
           )}
