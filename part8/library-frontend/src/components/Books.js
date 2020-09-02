0f08fe1db8af508e5d95310c00a6903c21191b58
Kristian Thy <thy@le34.dk>
2020-09-02 11:50:06 +0200

    8.19 Books by genre, part 1

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Books = (props) => {
+  const [genre, setGenre] = useState(null)
+
   if (!props.show) {
     return null
   }
@@ -9,9 +11,12 @@ const Books = (props) => {
     return <div>loading books...</div>
   }
 
+  const genres = new Set(props.result.data.allBooks.map(b => b.genres).flat())
+
   return (
     <div>
       <h2>books</h2>
+      {genre ? <h3>in genre {genre}</h3>: <h3>all genres</h3>}
       <table>
         <tbody>
           <tr>
@@ -23,7 +28,7 @@ const Books = (props) => {
               published
             </th>
           </tr>
-          {props.result.data.allBooks.map(b =>
+          {props.result.data.allBooks.filter(b => !genre || b.genres.includes(genre)).map(b =>
             <tr key={b.id}>
               <td>{b.title}</td>
               <td>{b.author.name}</td>
@@ -32,6 +37,10 @@ const Books = (props) => {
           )}
         </tbody>
       </table>
+      {[...genres].sort().map(g =>
+        <button key={g} onClick={() => setGenre(g)}>{g}</button>
+      )}
+      <button key='all' onClick={() => setGenre(null)}>all genres</button>
     </div>
   )
 }
